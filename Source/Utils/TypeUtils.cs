b8a827a11dc1a5cea54a17632e8fa8e25808e8ed
Chris Butler <chrisbutler@polygonhearts.com>
2022-12-22 00:29:13 +0000

    Adding Type Utils Class

---
+++ Source/Utils/TypeUtils.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace Vault;
+
+public static class TypeUtils
+{
+    public static Type[] GetAllTypesDerivedFromType<BaseType>(Assembly assembly, bool includeBaseTypeInOutput = false)
+    {
+        return assembly.GetTypes().Where(t =>
+        {
+            if(t == typeof(BaseType) && includeBaseTypeInOutput == false)
+            {
+                return false;
+            }
+            return typeof(BaseType).IsAssignableFrom(t);
+        }).ToArray();
+    }
+    
+    public static Type[] GetAllInterfacesDerivedFromType<BaseType>(Assembly assembly, bool includeBaseTypeInOutput = false)
+    {
+        return assembly.GetTypes().Where(t =>
+        {
+            if(t.IsInterface == false)
+            {
+                return false;
+            }
+            
+            if(t == typeof(BaseType) && includeBaseTypeInOutput == false)
+            {
+                return false;
+            }
+            return typeof(BaseType).IsAssignableFrom(t);
+        }).ToArray();
+    }
+}
